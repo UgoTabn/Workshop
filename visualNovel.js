fec8f1a2d3b97d391b61d39f2c119b62da9b5132
DamienLeMalETPA <59608514+DamienLeMalETPA@users.noreply.github.com>
2020-02-10 13:49:32 +0100

    changes

--- visualNovel.js
+++ visualNovel.js
@@ -1,14 +1,43 @@
-//
+/*
+//Var globales
 
-//Function
-function text(text){
-    
+function realisateur(index){
+	switch (index){
+		case 1 :
+			scene_1a
+			break;
+		case 2:
+
+			break;
+		case 3:
+
+			break;
+	}
 }
 
-function character(pos,state){
-
+function scene_1a(){
+	//change background
+	display_perso(2,"nom","state");
+	//senario
+	//dialogue("")
 }
 
-function scenetest(){
+function display_perso(pos,nom,state){
 
-}
+	$("#perso"+pos+"_img").attr("src","assets/character/"+nom+"_"+state+".png");
+	switch (pos){
+		case 1:
+			$("#perso1").css(/*position1 + flip*//*);
+			break;
+		case 2:
+			$("#perso2").css(/*position2 + flip*//*);
+			break;
+		case 3:
+			$("#perso3").css(/*position3 + flip*//*);
+			break;
+		case 4:
+			$("#perso4").css(/*position4 + flip*//*);
+			break;
+	}
+	
+}*/
