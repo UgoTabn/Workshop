23ba17419177fa021bcc512a85c57bae381fea6c
Dexeat <Dexeat@users.noreply.github.com>
2020-02-10 13:19:55 +0100

    v0.1

--- visualNovel.js
+++ visualNovel.js
@@ -2,13 +2,16 @@
 
 //Function
 function text(text){
-    
-}
-
-function character(pos,state){
 
 }
 
+function character(pos,state,who){
+    $('#perso'+pos).attr('src','assets/character/char'+who+'.png')
+
+}
 function scenetest(){
 
 }
+
+character(1,0,1);
+character(2,0,2);
