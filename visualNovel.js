92ccae05e14c9eaf7d4da97d8e08b6fb32a93fd3
Dexeat <Dexeat@users.noreply.github.com>
2020-02-10 15:19:58 +0100

    v0.2

--- visualNovel.js
+++ visualNovel.js
@@ -1,17 +1,55 @@
 //
-
+let nb = 1;
 //Function
 function text(text){
-
+    if (text == 'void'){
+        $('#text').html('')
+        $('#container').hide();
+    }
+    else{
+        $('#text').html(text);
+    }
 }
 
 function character(pos,state,who){
-    $('#perso'+pos).attr('src','assets/character/char'+who+'.png')
+    if(who == 0){
+        $('#perso'+pos).attr('src','assets/character/void.png')
+    }
+    else{$('#perso'+pos).attr('src','assets/character/char'+who+'.png')}
 
 }
 function scenetest(){
-
+    $('#background').css('background-image','url(assets/background/fondecranMaxime.png)')
+    character(1,0,1);
+    character(2,0,2);
+    text('Le texte marche car il a des pieds mdr');
 }
 
-character(1,0,1);
-character(2,0,2);
+function scenetest2(){
+    $('#background').css('background-image','url(assets/background/fond.jpg)')
+    character(1,0,2);
+    character(2,0,2);
+    text('OUI !')
+}
+
+function scenetest3(){
+    $('#background').css('background-image','url(assets/background/fondecranMaxime.png)')
+    character(1,0,0);
+    character(2,0,0);
+    text('LOL y\'a R');
+}
+
+scenetest();
+
+$('html').on('click',function(){
+    nb++;
+    console.log('VALIDE ! '+ nb)
+    switch(nb){
+        case 2:
+            scenetest2();
+            break;
+        case 3:
+            scenetest3();
+            break;
+    }       
+})
